85b0fd7c7ca550ec4352b1528a7bb164e7a615cb
Murilo Abreu <murilo_abr@yahoo.com.br>
2022-02-02 13:55:40 -0300

    1.0.0

---
+++ RestAspNET_sample01/Startup.cs
@@ -0,0 +1,27 @@
+﻿namespace RestAspNET_sample01 {
+    public class Startup {
+        public Startup(IConfiguration configuration) { 
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services) {
+            // Add services to the container.
+            services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+        public void Configure(WebApplication app, IWebHostEnvironment environment) {
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment()) {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+        }
+    }
+}
